9a88e336f4456ccd4013a14707dda3f1d5ae2f6f
Biaze7 <82425236+Biaze7@users.noreply.github.com>
2021-05-25 20:29:57 -0300

    Update autoencoder.py

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -32,13 +32,13 @@ class autoencoder(nn.Module):
         self.binarizer = Binarizer(64,128)
     def forward(self,x):
         
-         print(x.shape)
+        print(x.shape)
         x = self.enc(x)
-         print(x.shape) #AQUI
+        print(x.shape) #AQUI
         x = self.binarizer(x)
-         print(x.shape)
+        print(x.shape)
         x = self.dec(x)
-         print(x.shape)
+        print(x.shape)
     #     x = (x+1)*255
     #     x.round_()
         return x
