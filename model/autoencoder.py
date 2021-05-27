95763323a877bf9787b02d01cf0f498ade51a3ac
Biaze7 <82425236+Biaze7@users.noreply.github.com>
2021-05-27 18:58:18 -0300

    Update autoencoder.py

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
-from model.components import Binarizer
-
+#from model.components import Binarizer
+from model.components import *
 
 class autoencoder(nn.Module):
     def __init__(self):
